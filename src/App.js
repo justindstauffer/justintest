789c424ef1d111c540603981e844ef6f88adec87
Justin Stauffer <justindstauffer@gmail.com>
2021-03-22 16:38:11 -0400

    add jenkinsfile

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Justin's React Test
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Using Docker, Jenkins, and GitHub</p>
       </header>
     </div>
   );
